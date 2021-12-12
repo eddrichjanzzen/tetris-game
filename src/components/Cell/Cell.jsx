fc15528f2d130d6cdd50fc9c7a288757bc5a9d7c
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-12-12 21:21:13 +0800

    Add allow transparent functionality to cells

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import StyledCell from './Cell.styles';
 import { TETROMINOS } from '../../tetrominos';
 
-const Cell = ({ type }) => (
-  <StyledCell type={type} color={TETROMINOS[type].color} />
+const Cell = ({ type, allowTransparent = false }) => (
+  <StyledCell type={type} color={TETROMINOS[type].color} allowTransparent={allowTransparent} />
 );
 
 export default React.memo(Cell);
--- src/components/Cell/Cell.styles.jsx
+++ src/components/Cell/Cell.styles.jsx
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
+const isTransparent = (props) => (props.allowTransparent ? 0 : '0.1');
+const cellOpacity = (props) => (props.type === 0 ? (isTransparent) : '0.7');
+
 const StyledCell = styled.div`
   width: auto;
-  background: rgba(${(props) => props.color}, ${(props) => (props.type === 0 ? '0.1' : '0.7')});
-  border: ${(props) => (props.type === 0 ? '0px solid' : '10px solid')};
+  background: rgba(${(props) => props.color}, ${cellOpacity});
+  border: ${(props) => (props.type === 0 ? '0px solid' : '0.6vw solid')};
   border-color: rgba(${(props) => props.color});
 `;
 
