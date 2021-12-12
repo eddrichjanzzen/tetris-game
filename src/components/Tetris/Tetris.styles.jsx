e296641c633a8ce9d6bc9153991b2e7bc04bea68
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-12-12 17:21:06 +0800

    Update styles

--- src/components/Tetris/Tetris.styles.jsx
+++ src/components/Tetris/Tetris.styles.jsx
@@ -9,7 +9,6 @@ export const StyledTetrisWrapper = styled.div`
 
 export const StyledTetris = styled.div`
   display: flex;
-  align-items: flex-start;
   padding: 40px;
   margin: 0 auto;
   max-width: 900px;
